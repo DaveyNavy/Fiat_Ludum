c9e7f41ec11ad02c8e74e7b7cde4927b22407cc5
Sinkasha <xusherri@gmail.com>
2025-04-13 16:34:10 -0700

    Level 5

--- Assets/Scripts/Spikes.cs
+++ Assets/Scripts/Spikes.cs
@@ -9,7 +9,12 @@ public class Spikes : MonoBehaviour
             Debug.Log("player spiked");
             collision.GetComponent<PlayerBase>().DestroySelf();
 
-        } else if (collision.gameObject.tag == "Ground")
+        }
+        else if (collision.gameObject.tag == "Ground")
+        {
+            Destroy(gameObject);
+        }
+        else if (collision.gameObject.tag == "Breakable")
         {
             Destroy(gameObject);
         }
