523e162a199ab6305a4c94673abe19c034ab9468
DaveyNavy <davidyzhu2@gmail.com>
2025-04-13 21:37:22 -0700

    Fix bug where soul breaks when between two eggs

--- Assets/Scripts/Soul.cs
+++ Assets/Scripts/Soul.cs
@@ -78,7 +78,7 @@ public class Soul : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
